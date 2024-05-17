d829265b0693bc6d10ec43b746bbf1a4487ce609
sarkarnafe <sarkarnafe@gmail.com>
2024-05-18 05:54:05 +0600

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np 
   
 # Read image. 
-img = cv2.imread('F:\CodeBase\IOT_OMR_SCANNER\img\Accurecy_testing.png', cv2.IMREAD_COLOR) 
+img = cv2.imread('G:\CodeBase\IOT_OMR_SCANNER\img\Accurecy_testing.png', cv2.IMREAD_COLOR) 
 
 # Convert to grayscale. 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
